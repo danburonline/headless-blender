165d0b638a241f85c8298eb8781029921dcc25c9
Daniel Burger <public@danielburger.online>
2023-12-03 19:17:00 +0100

    Tried to make the self-fracture work with a GLTF

--- src/self_fracture.py
+++ src/self_fracture.py
@@ -1,50 +1,46 @@
-"""Example Python headless file"""
 import os
 import bpy
 
-# Create an empty Blender file
-bpy.ops.wm.read_factory_settings(use_empty=True)
+# Load a GLTF file
+def load_gltf(file_path):
+    bpy.ops.import_scene.gltf(filepath=file_path)
 
-# Create a red sphere in the center of the scene
-bpy.ops.mesh.primitive_uv_sphere_add(
-    radius=1, enter_editmode=False, align="WORLD", location=(0, 0, 0)
-)
+gltf_file_path = './src/scene.gltf'
+load_gltf(gltf_file_path)
 
-# Set the sphere's material to red
-material = bpy.data.materials.new(name="RedMaterial")
-material.diffuse_color = (1, 0, 0, 1)  # Red color
-bpy.context.object.data.materials.append(material)
+imported_object = None
+for obj in bpy.data.objects:
+    if obj.type == 'MESH':
+        imported_object = obj
+        break
+
+if not imported_object:
+    raise Exception("No imported mesh found")
+
+# Move the object up
+imported_object.location.z += 3.0
 
 # Ensure the Cell Fracture addon is enabled
 bpy.ops.preferences.addon_enable(module="object_fracture_cell")
 
+# Make the imported object the active object
+bpy.context.view_layer.objects.active = imported_object
+
+# Apply Cell Fracture
+bpy.ops.object.add_fracture_cell_objects()
+
+# Store the name of the original object
+original_obj_name = imported_object.name
+
 # Deselect all objects
 bpy.ops.object.select_all(action="DESELECT")
 
-# Process each object in the scene
+# Select and delete the original object
+bpy.data.objects[original_obj_name].select_set(True)
+bpy.ops.object.delete()
+
+# Iterate over fractured pieces to add rigid body physics
 for obj in bpy.data.objects:
-    obj.select_set(True)
-    obj.location.z += 3.0
-
-    # Make the object the active object
-    bpy.context.view_layer.objects.active = obj
-
-    # Apply Cell Fracture
-    bpy.ops.object.add_fracture_cell_objects()
-
-    # Store the name of the original object
-    original_obj_name = obj.name
-
-    # Deselect all objects
-    bpy.ops.object.select_all(action="DESELECT")
-
-    # Select and delete the original object
-    bpy.data.objects[original_obj_name].select_set(True)
-    bpy.ops.object.delete()
-
-# Iterate
-for obj in bpy.data.objects:
-    # Check if the object is a fractured piece
     if original_obj_name in obj.name:
         obj.select_set(True)
         bpy.context.view_layer.objects.active = obj
@@ -52,9 +48,7 @@ for obj in bpy.data.objects:
         obj.rigid_body.type = "ACTIVE"
 
 # Add a plane at the center
-bpy.ops.mesh.primitive_plane_add(
-    size=2, enter_editmode=False, align="WORLD", location=(0, 0, 0)
-)
+bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align="WORLD", location=(0, 0, 0))
 
 # Scale up the plane
 plane = bpy.context.active_object
@@ -72,7 +66,6 @@ bpy.ops.ptcache.bake_all(bake=True)
 for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end + 1):
     bpy.context.scene.frame_set(frame)
     for obj in bpy.data.objects:
-        # Only apply to fractured pieces
         if original_obj_name in obj.name:
             bpy.context.view_layer.objects.active = obj
             bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
