4ebbff364d6dc3ffe3dbc92c8030ef59bce53200
Daniel Burger <public@danielburger.online>
2023-12-09 20:45:23 +0100

    Turn the Python script into a web server

--- src/self_fracture.py
+++ src/self_fracture.py
@@ -4,160 +4,152 @@ from datetime import datetime
 import bpy
 
 
-# Create an empty Blender file
-bpy.ops.wm.read_factory_settings(use_empty=True)
-
-
-def load_gltf(gltf_file_path):
-    """Load a GLTF file"""
+def process_gltf(gltf_file_path):
+    """Process a GLTF file"""
+    # Create an empty Blender file
+    bpy.ops.wm.read_factory_settings(use_empty=True)
     bpy.ops.import_scene.gltf(filepath=gltf_file_path)
 
+    imported_object = None
+    for obj in bpy.data.objects:
+        if obj.type == "MESH":
+            imported_object = obj
+            break
 
-GLTF_FILE_PATH = "./src/scene.gltf"
-load_gltf(GLTF_FILE_PATH)
+    if not imported_object:
+        raise ValueError("No imported mesh found")
 
-IMPORTED_OBJECT = None
-for obj in bpy.data.objects:
-    if obj.type == "MESH":
-        IMPORTED_OBJECT = obj
-        break
+    # Move the object up
+    x, y, z = imported_object.location
+    imported_object.location = (x, y, z + 2.0)
 
-if not IMPORTED_OBJECT:
-    raise ValueError("No imported mesh found")
+    # Apply the scale
+    bpy.ops.object.transform_apply(scale=True)
 
-# Move the object up
-x, y, z = IMPORTED_OBJECT.location
-IMPORTED_OBJECT.location = (x, y, z + 2.0)
-
-# Apply the scale
-bpy.ops.object.transform_apply(scale=True)
-
-# Ensure the Cell Fracture addon is enabled
-bpy.ops.preferences.addon_enable(module="object_fracture_cell")
-
-# Deselect all objects
-bpy.ops.object.select_all(action="DESELECT")
-
-# Process each object in the scene
-for obj in bpy.data.objects:
-    obj.select_set(True)
-    x, y, z = obj.location
-    obj.location = (x, y, z + 3.0)
-
-    # Make the object the active object
-    bpy.context.view_layer.objects.active = obj
-
-    # Apply Cell Fracture
-    # pylint: disable=no-member
-    bpy.ops.object.add_fracture_cell_objects()  # type: ignore
-
-    # Store the name of the original object
-    original_obj_name = obj.name
+    # Ensure the Cell Fracture addon is enabled
+    bpy.ops.preferences.addon_enable(module="object_fracture_cell")
 
     # Deselect all objects
     bpy.ops.object.select_all(action="DESELECT")
 
-    # Select and delete the original object
-    bpy.data.objects[original_obj_name].select_set(True)
-    bpy.ops.object.delete()
+    # Process each object in the scene
+    for obj in bpy.data.objects:
+        obj.select_set(True)
+        x, y, z = obj.location
+        obj.location = (x, y, z + 3.0)
 
-# Find the first mesh object, which will be your imported GLTF, to set as active
-for obj in bpy.data.objects:
-    if obj.type == "MESH":
-        bpy.context.view_layer.objects.active = obj  # Set the mesh object as active
-        break
+        # Make the object the active object
+        bpy.context.view_layer.objects.active = obj
 
-if bpy.context.active_object is not None:
-    original_obj_name = bpy.context.active_object.name
-else:
-    raise ValueError(
-        "No active mesh object found. Please ensure the GLTF file was imported correctly."
+        # Apply Cell Fracture
+        # pylint: disable=no-member
+        bpy.ops.object.add_fracture_cell_objects()  # type: ignore
+
+        # Store the name of the original object
+        original_obj_name = obj.name
+
+        # Deselect all objects
+        bpy.ops.object.select_all(action="DESELECT")
+
+        # Select and delete the original object
+        bpy.data.objects[original_obj_name].select_set(True)
+        bpy.ops.object.delete()
+
+    # Find the first mesh object, which will be your imported GLTF, to set as active
+    for obj in bpy.data.objects:
+        if obj.type == "MESH":
+            bpy.context.view_layer.objects.active = obj  # Set the mesh object as active
+            break
+
+    if bpy.context.active_object is not None:
+        original_obj_name = bpy.context.active_object.name
+    else:
+        raise ValueError(
+            "No active mesh object found. Please ensure the GLTF file was imported correctly."
+        )
+
+    # Apply rigid body to fractured pieces
+    for obj in bpy.data.objects:
+        # The fractured pieces should have the original object's name as part of their new name
+        if original_obj_name in obj.name:
+            bpy.ops.object.select_all(action="DESELECT")  # Deselect all objects
+            obj.select_set(True)  # Select the current object
+            bpy.context.view_layer.objects.active = obj  # Set as the active object
+            bpy.ops.rigidbody.object_add()  # Add active rigid body
+            obj.rigid_body.type = "ACTIVE"  # Set rigid body to active
+            obj.rigid_body.collision_shape = "MESH"  # Set collision shape to MESH
+
+    # Add a plane at the center
+    bpy.ops.mesh.primitive_plane_add(
+        size=2, enter_editmode=False, align="WORLD", location=(0, 0, 0)
     )
 
+    # Scale up the plane
+    plane = bpy.context.active_object
+    plane.scale = (5, 5, 5)
 
-# Apply rigid body to fractured pieces
-for obj in bpy.data.objects:
-    # The fractured pieces should have the original object's name as part of their new name
-    if original_obj_name in obj.name:
-        bpy.ops.object.select_all(action="DESELECT")  # Deselect all objects
-        obj.select_set(True)  # Select the current object
-        bpy.context.view_layer.objects.active = obj  # Set as the active object
-        bpy.ops.rigidbody.object_add()  # Add active rigid body
-        obj.rigid_body.type = "ACTIVE"  # Set rigid body to active
-        obj.rigid_body.collision_shape = "MESH"  # Set collision shape to MESH
+    # Set plane as passive rigid body
+    bpy.ops.rigidbody.object_add()
+    plane.rigid_body.type = "PASSIVE"
+    plane.rigid_body.collision_shape = "MESH"
 
+    # Bake physics
+    bpy.ops.ptcache.bake_all(bake=True)
 
-# Add a plane at the center
-bpy.ops.mesh.primitive_plane_add(
-    size=2, enter_editmode=False, align="WORLD", location=(0, 0, 0)
-)
+    # Apply visual transform to each object and insert keyframes
+    for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end + 1):
+        bpy.context.scene.frame_set(frame)
+        for obj in bpy.data.objects:
+            # Only apply to fractured pieces
+            if original_obj_name in obj.name:
+                bpy.context.view_layer.objects.active = obj
+                bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+                obj.keyframe_insert(data_path="location")
+                obj.keyframe_insert(data_path="rotation_euler")
+                obj.keyframe_insert(data_path="scale")
 
-# Scale up the plane
-plane = bpy.context.active_object
-plane.scale = (5, 5, 5)
+    # Get current date and time
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# Set plane as passive rigid body
-bpy.ops.rigidbody.object_add()
-plane.rigid_body.type = "PASSIVE"
-plane.rigid_body.collision_shape = "MESH"
+    # Define the file path with the current date and time appended
+    file_path = os.path.join(
+        os.path.dirname(bpy.data.filepath), f"dist/example_{current_datetime}.blend"
+    )
 
-# Bake physics
-bpy.ops.ptcache.bake_all(bake=True)
+    # Save the file
+    bpy.ops.wm.save_as_mainfile(filepath=file_path)
 
-# Apply visual transform to each object and insert keyframes
-for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end + 1):
-    bpy.context.scene.frame_set(frame)
-    for obj in bpy.data.objects:
-        # Only apply to fractured pieces
-        if original_obj_name in obj.name:
-            bpy.context.view_layer.objects.active = obj
-            bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-            obj.keyframe_insert(data_path="location")
-            obj.keyframe_insert(data_path="rotation_euler")
-            obj.keyframe_insert(data_path="scale")
+    # Remove the plane
+    bpy.data.objects.remove(plane, do_unlink=True)
 
-# Get current date and time
-current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # Get the directory where the blend file is located
+    blend_file_directory = os.path.dirname(bpy.data.filepath)
 
-# Define the file path with the current date and time appended
-file_path = os.path.join(
-    os.path.dirname(bpy.data.filepath), f"dist/example_{current_datetime}.blend"
-)
+    # If the blend file is not saved, use the current directory
+    if not blend_file_directory:
+        blend_file_directory = os.path.dirname(os.path.realpath(__file__))
 
-# Save the file
-bpy.ops.wm.save_as_mainfile(filepath=file_path)
+    # Ensure the 'dist' directory exists
+    dist_directory = os.path.join(blend_file_directory, "dist")
+    os.makedirs(dist_directory, exist_ok=True)
 
-# Remove the plane
-bpy.data.objects.remove(plane, do_unlink=True)
+    # Get current date and time for the file name
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# Get the directory where the blend file is located
-blend_file_directory = os.path.dirname(bpy.data.filepath)
+    # Define the GLB export path with the current date and time
+    glb_export_path = os.path.join(
+        dist_directory, f"animated_fracture_{current_datetime}.glb"
+    )
 
-# If the blend file is not saved, use the current directory
-if not blend_file_directory:
-    blend_file_directory = os.path.dirname(os.path.realpath(__file__))
+    # Export to GLB with animations
+    bpy.ops.export_scene.gltf(
+        filepath=glb_export_path,
+        export_format="GLB",
+        use_selection=False,  # Export all objects in the scene
+        export_apply=True,  # Apply modifiers and other settings
+        export_animations=True,  # Include animations
+        export_frame_range=True,  # Use the current frame range
+        export_frame_step=1,  # Export all frames
+    )
 
-# Ensure the 'dist' directory exists
-dist_directory = os.path.join(blend_file_directory, "dist")
-os.makedirs(dist_directory, exist_ok=True)
-
-# Get current date and time for the file name
-current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-# Define the GLB export path with the current date and time
-glb_export_path = os.path.join(
-    dist_directory, f"animated_fracture_{current_datetime}.glb"
-)
-
-# Export to GLB with animations
-bpy.ops.export_scene.gltf(
-    filepath=glb_export_path,
-    export_format="GLB",
-    use_selection=False,  # Export all objects in the scene
-    export_apply=True,  # Apply modifiers and other settings
-    export_animations=True,  # Include animations
-    export_frame_range=True,  # Use the current frame range
-    export_frame_step=1,  # Export all frames
-)
-
-print(f"Exported GLB file to {glb_export_path}")
+    print(f"Exported GLB file to {glb_export_path}")
---
+++ src/server.py
@@ -0,0 +1,34 @@
+"""Web server for uploading GLTF files and processing them with Blender."""
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import os
+
+from src.self_fracture import process_gltf
+from zipfile import ZipFile
+
+app = FastAPI()
+
+
+@app.post("/upload-gltf/")
+async def upload_gltf(zip_file: UploadFile = File(...)):
+    """Upload a GLTF file and process it with Blender."""
+    temp_dir = f"temp/{zip_file.filename}-unzipped"
+    os.makedirs(temp_dir, exist_ok=True)
+    temp_zip_path = f"{temp_dir}/{zip_file.filename}"
+    with open(temp_zip_path, "wb") as buffer:
+        shutil.copyfileobj(zip_file.file, buffer)
+
+    # Unzip the file
+    with ZipFile(temp_zip_path, "r") as zip_ref:
+        zip_ref.extractall(temp_dir)
+
+    # Assume the GLTF file is named 'scene.gltf'
+    gltf_file_path = os.path.join(temp_dir, "scene.gltf")
+
+    # Process the GLTF file with your Blender function
+    process_gltf(gltf_file_path)
+
+    # Clean up: remove the temporary directory
+    shutil.rmtree(temp_dir)
+
+    return {"filename": zip_file.filename}
