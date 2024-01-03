3b8ef1af302cb4fad3bd73a34f493d2ec633ed77
Daniel Burger <public@danielburger.online>
2024-01-03 21:03:21 +0100

    Refactor new Blender script with timestamp etc.

--- src/generic_deform.py
+++ src/generic_deform.py
@@ -1,7 +1,17 @@
 import bpy
+import os
+from datetime import datetime
+
+# Base directory assuming the script is run from the root directory
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the source and destination paths
+source_blend = os.path.join(base_dir, "..", "files", "blender", "example_source.blend")
+destination_folder = os.path.join(base_dir, "..", "dist")
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
 # Load the .blend file
-bpy.ops.wm.open_mainfile(filepath="example.blend")
+bpy.ops.wm.open_mainfile(filepath=source_blend)
 
 # Deselect all objects first to ensure a clean slate
 bpy.ops.object.select_all(action="DESELECT")
@@ -16,15 +26,22 @@ bpy.context.view_layer.objects.active = obj
 # Scale the object
 bpy.ops.transform.resize(value=(2.76192, 2.76192, 2.76192), orient_type="GLOBAL")
 
-# switch into edit mode
+# Switch into edit mode
 bpy.ops.object.editmode_toggle()
 
-# select a random vertex
-# TODO This doesn't work
+# Select a random vertex
+# TODO This doesn't work from here on
 bpy.ops.mesh.select_random(ratio=0.5, seed=0, action="SELECT")
 
-# move this vertex randomly in 3d space
+# Move this vertex randomly in 3d space
 bpy.ops.transform.translate(value=(0.0, 0.0, 0.0), orient_type="GLOBAL")
 
-# Save the changes to a new .blend file
-bpy.ops.wm.save_as_mainfile(filepath="modified_example.blend")
+# Make sure the destination folder exists
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
+
+# Save the changes to a new .blend file with a timestamp
+output_blend = os.path.join(
+    destination_folder, f"transformed_example_{timestamp}.blend"
+)
+bpy.ops.wm.save_as_mainfile(filepath=output_blend)
